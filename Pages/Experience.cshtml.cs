a1345bec61fc895b6490344dc689106150ae2c8c
Daniel Corbi Boldrin <daniel_cboldrin@hotmail.com>
2021-06-17 15:59:00 -0400

    Added Experience Page
    Created First version of Experience page.
    Added all my previous jobs and descriptions.
    Added custom technologies icons.
    Created icon hover css animation.

---
+++ Pages/Experience.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Pages
+{
+    public class ExperienceModel : PageModel
+    {
+        private readonly ILogger<ExperienceModel> _logger;
+
+        public ExperienceModel(ILogger<ExperienceModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
