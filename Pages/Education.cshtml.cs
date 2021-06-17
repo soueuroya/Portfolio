27f9cb9040a5dc60ad4ab1aef1ce5126aecd2615
Daniel Corbi Boldrin <daniel_cboldrin@hotmail.com>
2021-06-17 16:03:44 -0400

    Created Education Page
    Created first version of education page.
    Added all my previous schools and certifications.

---
+++ Pages/Education.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Pages
+{
+    public class EducationModel : PageModel
+    {
+        private readonly ILogger<EducationModel> _logger;
+
+        public EducationModel(ILogger<EducationModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
