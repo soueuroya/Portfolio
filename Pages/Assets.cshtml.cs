3b1dd6c19f9169bcddf1f4b13f0b45fd2a41beca
DanielCBoldrin <daniel_cboldrin@hotmail.com>
2022-05-21 17:55:12 -0400

    Updated with assets page
    Added Match3 and Lockpicking minigames.
    Added a few Black and white icons
    Updates colorise pngs.
    converted a few jpgs into pngs.
    Updated resumes.

---
+++ Pages/Assets.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Pages
+{
+    public class AssetsModel : PageModel
+    {
+        private readonly ILogger<AssetsModel> _logger;
+
+        public AssetsModel(ILogger<AssetsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
